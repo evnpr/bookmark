f3012d9e10c55de6d0017a75dfa15c341be22bcb
evanpy <evanpurnama5@gmail.com>
2012-07-27 07:51:49 +0800

    safas

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-
+require 'select_with_include'
 # Initialize the rails application
 Bookmark::Application.initialize!
