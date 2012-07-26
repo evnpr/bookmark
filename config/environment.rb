e0a464cfaa248bb6ef9680c4102b0d052fdbb121
evanpy <evanpurnama5@gmail.com>
2012-07-27 07:56:59 +0800

    assaf

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,4 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-require 'select_with_include'
 # Initialize the rails application
 Bookmark::Application.initialize!
