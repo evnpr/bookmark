4cc5f192268a18bcc82098177d9bd63f8314f9a6
evanpy <evanpurnama5@gmail.com>
2012-07-27 10:23:54 +0800

    put bootstrap

---
+++ app/app/controllers/profile_controller.rb
@@ -0,0 +1,31 @@
+class ProfileController < ApplicationController
+
+  def new
+  end
+
+  def signup
+        @p = Profile.new(:username => params[:username])
+        @p.password = params[:password]
+        @p.save
+        redirect_to "/"
+  end
+
+  def index
+  end
+
+  def sign
+    @p = Profile.where(:username => params[:username], :password => params[:password]).first
+    if(!@p.nil?)
+        session[:username] = params[:username]
+        redirect_to "/sites"
+    else
+        redirect_to "/"
+    end
+  end
+
+  def logout
+        session[:username]=''
+        redirect_to "/"
+  end
+
+end
