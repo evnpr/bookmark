5c29f5867bdef537d5ecf0b461d8cdebea46a970
evanpy <evanpurnama5@gmail.com>
2012-07-30 13:16:15 +0800

    bug fixed

--- app/models/site.rb
+++ app/models/site.rb
@@ -2,7 +2,9 @@ class Site < ActiveRecord::Base
     belongs_to :profile
     belongs_to :category
 
-    def category_name(id)
-       Category.find(id).title 
+    def category_name
+       if(!category_id.nil?) 
+            Category.find(self.category_id).title 
+       end
     end
 end
